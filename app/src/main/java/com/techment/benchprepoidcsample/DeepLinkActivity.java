39c6f5f5a3b4726629d8c325b637052f69167b81
rajendratechment <rajendra.verma@techment.com>
2022-11-24 00:48:32 +0530

    First commit

---
+++ app/src/main/java/com/techment/benchprepoidcsample/DeepLinkActivity.java
@@ -0,0 +1,30 @@
+package com.techment.benchprepoidcsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DeepLinkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deep_link);
+
+     String accessToken =   getIntent().getData().getQueryParameter("accessToken");
+     TextView accessTokenTv =findViewById(R.id.accessToken);
+
+     if (accessToken ==null || accessToken.isEmpty()){
+         accessTokenTv.setText("Authentication Failure");
+     }else {
+         accessTokenTv.setText("Access Token:: \n"+accessToken);
+     }
+
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/techment/benchprepoidcsample/MainActivity.java
@@ -0,0 +1,67 @@
+package com.techment.benchprepoidcsample;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.techment.benchprepoidcsample.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.magic_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://quick-start-demo-9314f.web.app/";
+
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(browserIntent);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
