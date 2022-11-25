5335a160e6f9add4c23b5f3222decbf6eddf7760
rajendratechment <rajendra.verma@techment.com>
2022-11-25 21:29:49 +0530

    implement the dynamic link firebase

--- app/src/main/java/com/techment/benchprepoidcsample/DeepLinkActivity.java
+++ app/src/main/java/com/techment/benchprepoidcsample/DeepLinkActivity.java
@@ -1,19 +1,55 @@
 package com.techment.benchprepoidcsample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
-public class DeepLinkActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
+import com.google.firebase.dynamiclinks.PendingDynamicLinkData;
 
+public class DeepLinkActivity extends AppCompatActivity {
+    String TAG ="DeepLinkActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deep_link);
+        FirebaseDynamicLinks.getInstance()
+                .getDynamicLink(getIntent())
+                .addOnSuccessListener(this, new OnSuccessListener<PendingDynamicLinkData>() {
+                    @Override
+                    public void onSuccess(PendingDynamicLinkData pendingDynamicLinkData) {
+                        // Get deep link from result (may be null if no link is found)
+                        Uri deepLink = null;
+                        if (pendingDynamicLinkData != null) {
+                            deepLink = pendingDynamicLinkData.getLink();
+                        }
 
-     String accessToken =   getIntent().getData().getQueryParameter("accessToken");
+
+                        // Handle the deep link. For example, open the linked
+                        // content, or apply promotional credit to the user's
+                        // account.
+                        // ...
+
+                        // ...
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                        Log.w(TAG, "getDynamicLink:onFailure", e);
+                    }
+                });
+
+
+        String accessToken =   getIntent().getData().getQueryParameter("accessToken");
      TextView accessTokenTv =findViewById(R.id.accessToken);
 
      if (accessToken ==null || accessToken.isEmpty()){
--- app/src/main/java/com/techment/benchprepoidcsample/MainActivity.java
+++ app/src/main/java/com/techment/benchprepoidcsample/MainActivity.java
@@ -4,10 +4,14 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -15,12 +19,16 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
+import com.google.firebase.dynamiclinks.PendingDynamicLinkData;
 import com.techment.benchprepoidcsample.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    String TAG ="DeepLinkActivity";
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
@@ -29,6 +37,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDynamicLinks.getInstance()
+                .getDynamicLink(getIntent())
+                .addOnSuccessListener(this, new OnSuccessListener<PendingDynamicLinkData>() {
+                    @Override
+                    public void onSuccess(PendingDynamicLinkData pendingDynamicLinkData) {
+                        // Get deep link from result (may be null if no link is found)
+                        Uri deepLink = null;
+                        if (pendingDynamicLinkData != null) {
+                            deepLink = pendingDynamicLinkData.getLink();
+                            String accessToken =   deepLink.getQueryParameter("accesstoken");
+                            TextView accessTokenTv =findViewById(R.id.accessToken);
+
+                            if (accessToken ==null || accessToken.isEmpty()){
+                                accessTokenTv.setText("Authentication Failure");
+                            }else {
+                                accessTokenTv.setText("Access Token:: \n"+accessToken);
+                            }
+                        }
+
+
+                        // Handle the deep link. For example, open the linked
+                        // content, or apply promotional credit to the user's
+                        // account.
+                        // ...
+
+                        // ...
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                        Log.w(TAG, "getDynamicLink:onFailure", e);
+                    }
+                });
+
         findViewById(R.id.magic_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
